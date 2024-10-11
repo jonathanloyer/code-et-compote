c81a209c2a00bcd0c9fc5d5b1075a1d574a2cb77
jochouca <jonathanloyer1@gmail.com>
2024-10-11 11:32:58 +0200

    three

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -24,8 +24,22 @@ class AdminUserController extends AbstractController
     }
     public function showUpdateUserForm($params)
     {
+        $session = new Session();
+
+        if ($session->isAdmin() === false) {
+            header('Location:/code-et-compote/');
+            exit;
+        }
         $id = $params['id'];
 
-        $this->render('admin/updateUserForm', ['id'=> $id]);
+        // Vérification si la variable n'est pas un entier
+        if (filter_var($id, FILTER_VALIDATE_INT) === false) {
+            // Redirection vers /code-et-compote/admin/utilisateurs si ce n'est pas un entier
+            header("Location: /code-et-compote/admin/utilisateurs");
+            exit(); // Toujours appeler exit() après une redirection pour stopper l'exécution du script
+        }
+
+
+        $this->render('admin/updateUserForm', ['id' => $id, 'user' => $user]);
     }
 }
--- templates/admin/updateUserForm.php
+++ templates/admin/updateUserForm.php
@@ -1,4 +1,15 @@
 <form action="">
-
-
+    <div class="mb-3">
+        <label for="pseudo">Pseudo</label>
+        <input type="text" id="pseudo" name="pseudo" class="form-control" value="<?= $user['username']; ?>">
+    </div>
+    <div class="mb-3">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <lab class="mb-3" el for="pswd">Mot de passe</lab>
+        <input type="password" id="pswd" name="pswd" class="form-control">
+    </div>
+    <input type="submit" value="Modifier" class="btn btn-primary">
 </form>
